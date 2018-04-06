9244685c757cb1e598a15e2687fcb5d4c38fbd31
Douglas_HdezT <00004516@uca.edu.sv>
2018-04-05 19:47:42 -0600

    Parte grafica

--- app/src/main/java/com/debugprojects/poweredby/MainActivity.java
+++ app/src/main/java/com/debugprojects/poweredby/MainActivity.java
@@ -2,8 +2,13 @@ package com.debugprojects.poweredby;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declarando variables de control
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
